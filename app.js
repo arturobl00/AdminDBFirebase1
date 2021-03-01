a8604840ed799e83fabac5bbda8ff194d4dca31b
Sistemas y Telecomunicaciones Tehuacán <44816412+arturobl00@users.noreply.github.com>
2021-03-01 12:49:20 -0600

    Modulo de mostrar

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ firebase.initializeApp({
   
   var db = firebase.firestore();
 
+  //Codigo para agregar registros a mi colección
   function agregar(){
       var nombre = document.getElementById('nombre').value;
       var apellido = document.getElementById('apellido').value;
@@ -20,9 +21,32 @@ firebase.initializeApp({
         })
         .then(function(docRef) {
             console.log("Document written with ID: ", docRef.id);
+            document.getElementById('nombre').value = '';
+            document.getElementById('apellido').value = '';
+            document.getElementById('año').value = '';
         })
         .catch(function(error) {
             console.error("Error adding document: ", error);
         });
   }
-  
+
+  //Codigo para Leer o Mostrar registros de mi colección
+  //Leer el id de la tabla
+  var tabla = document.getElementById('tabla');
+
+  db.collection("users").onSnapshot((querySnapshot) => {
+      tabla.innerHTML = ''; //Limpiar mi tabla
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().last}`);
+        tabla.innerHTML += `
+        <tr>
+            <th scope="row">${doc.id}</th>
+            <td>${doc.data().first}</td>
+            <td>${doc.data().last}</td>
+            <td>${doc.data().born}</td>
+            <td><button class="btn btn-danger">Eliminar</button></td>
+            <td><button class="btn btn-warning">Editar</button></td>
+        </tr>
+        `
+        });
+    });
